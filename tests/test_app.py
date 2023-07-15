2b395d98d4bae17cc9dac5bbc8d44bd0f5fcb2c5
CS-20201106LNMF\Administrator <B122712154@gmail.com>
2023-07-15 10:45:11 +0800

    feat: 測試程式

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,49 @@
 import unittest
 from app import create_app
+from flask_testing import TestCase
+import json
 
-class MyTestCase(unittest.TestCase):
-    # def test_something(self):
-    #     self.assertEqual(True, False)  # add assertion here
-
-    def setUp(self):
+class MyTestCase(TestCase):
+    def create_app(self):
         app = create_app()
-        self.app = app.test_client()
-
+        return app
     def test_exchane_rate(self):
-        query_string = {'source': 'USD', 'target': 'JPY', 'amount': '100'}
-        response = self.app.get('/exchane_rate', query_string=query_string)
+        app = self.create_app()
+        with self.app.test_client() as client:
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"msg": "success", "amount": "10,800.00"})
+            query_string = {'source': 'USD', 'target': 'JPY', 'amount': '100'}
 
+            response = client.get('/exchane_rate', query_string=query_string)
+            response_data = response.data.decode('utf-8')
 
+            #回傳狀態碼是否為200
+            self.assertEqual(response.status_code, 200)
+            #回傳內容是否為預期結果
+            self.assertEqual(json.loads(response_data), {'msg': 'success', 'amount': '11,180.10'})
+            # print(response.json)
+
+    def test_exchane_rate_missing_parameters(self):
+        app = self.create_app()
+        with self.app.test_client() as client:
+
+            query_string = {'source': 'USD', 'target': 'JPY'}
+
+            response = client.get('/exchane_rate', query_string=query_string)
+            response_data = response.data.decode('utf-8')
+
+            #回傳內容是否為預期結果
+            self.assertEqual(json.loads(response_data), {'error': 'Missing parameters'})
+
+    def test_exchane_rate_invalid_amount(self):
+        app = self.create_app()
+        with self.app.test_client() as client:
+
+            query_string = {'source': 'USD', 'target': 'JPY', 'amount': 'test'}
+
+            response = client.get('/exchane_rate', query_string=query_string)
+            response_data = response.data.decode('utf-8')
+
+            #回傳內容是否為預期結果
+            self.assertEqual(json.loads(response_data), {'error': 'Invalid amount'})
 if __name__ == '__main__':
     unittest.main()
